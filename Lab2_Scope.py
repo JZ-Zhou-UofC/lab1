82dfa9de24770a8ba824d68dd2032201c7c85b31
John Zhou <john.zhou1@ucalgary.ca>
2025-05-08 09:59:09 -0600

    first commit

---
+++ Lab2_Scope.py
@@ -0,0 +1,89 @@
+# ENSF 692 Spring 2025
+# May 8 Lab 2
+# Exercise 2
+
+
+# Trace through the execution of the following program. 
+# Answer the questions in the comments with your group members.
+# After discussing, use print statements to confirm your answers.
+
+foo = 100
+bar = foo
+foo + bar
+print("pint1")
+print("fool Value", foo)
+print("fool type", type(foo))  
+print("bar Value", bar)
+print("bar type", type(bar))
+# POINT 1
+# What is the value of foo at this point?
+# What is the type of foo at this point?
+# What is the value of bar at this point?
+# What is the type of bar at this point?
+
+spam = foo + bar
+foo += 50
+eggs = foo + bar
+ham = [1, 2, 3]
+baz = ham
+ham.append(bar)
+
+# POINT 2
+# What is the value of foo at this point?
+# What is the value of bar at this point?
+# What is the value of spam at this point?
+# What is the value of eggs at this point?
+# What is the value of ham at this point?
+# What is the value of baz at this point?
+print("point2")
+print("fool Value", foo)
+
+print("bar Value", bar)
+
+print("bar Value", bar)
+
+print("bar Value", bar)
+
+print("spam Value", spam)
+
+print("eggs Value", eggs)
+
+print("ham Value", ham)
+
+print("baz Value", baz)
+
+eggs = "Python is very flexible!"
+spam = ham
+ham = bar
+bar += bar
+foo = eggs
+eggs = bar + ham
+baz.append(bar)
+
+
+
+# POINT 3
+# What is the value of foo at this point?
+# What is the value of bar at this point?
+# What is the value of spam at this point?
+# What is the value of eggs at this point?
+# What is the value of ham at this point?
+# What is the value of baz at this point?
+
+# Print out the types and final values of each variable.
+print("point3")
+print("fool Value", foo)
+
+print("bar Value", bar)
+
+print("bar Value", bar)
+
+print("bar Value", bar)
+
+print("spam Value", spam)
+
+print("eggs Value", eggs)
+
+print("ham Value", ham)
+
+print("baz Value", baz)
